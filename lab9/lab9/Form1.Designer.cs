125d7c02dae91f002e709c48fe196f52a28f3057
vlasenkoi <159702406+vlasenkoi@users.noreply.github.com>
2025-05-14 11:21:09 +0300

    Add files via upload

---
+++ lab9/lab9/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxOutput = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxOutput
+            // 
+            this.picBoxOutput.Location = new System.Drawing.Point(252, 27);
+            this.picBoxOutput.Name = "picBoxOutput";
+            this.picBoxOutput.Size = new System.Drawing.Size(385, 356);
+            this.picBoxOutput.TabIndex = 0;
+            this.picBoxOutput.TabStop = false;
+            this.picBoxOutput.Paint += new System.Windows.Forms.PaintEventHandler(this.picBoxOutput_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A = ";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(80, 46);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 22);
+            this.txtA.TabIndex = 2;
+            this.txtA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtA_KeyPress);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(80, 151);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(76, 42);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 401);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBoxOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ lab9/lab9/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            picBoxOutput.Paint += new PaintEventHandler(this.picBoxOutput_Paint);
+        }
+
+        float a = 1;
+       
+        private void picBoxOutput_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int width = picBoxOutput.Width;
+            int height = picBoxOutput.Height;
+
+            // Центр координат
+            int centerX = width / 2;
+            int centerY = height / 2;
+
+            Pen axisPen = new Pen(Color.Black, 2);
+            Pen graphPen = new Pen(Color.Blue, 2);
+            Font font = new Font("Arial", 10);
+
+            // Малюємо осі
+            g.DrawLine(axisPen, 0, centerY, width, centerY); // Ox
+            g.DrawLine(axisPen, centerX, 0, centerX, height); // Oy
+
+            // Малюємо поділки та підписи
+            for (int i = -10; i <= 10; i++)
+            {
+                int x = centerX + i * 40;
+                int y = centerY - i * 40;
+
+                // поділки по X
+                g.DrawLine(Pens.Black, x, centerY - 5, x, centerY + 5);
+                g.DrawString(i.ToString(), font, Brushes.Black, x - 10, centerY + 10);
+
+                // поділки по Y
+                g.DrawLine(Pens.Black, centerX - 5, y, centerX + 5, y);
+                if (i != 0)
+                    g.DrawString((-i).ToString(), font, Brushes.Black, centerX + 10, y - 10);
+            }
+
+            // Малюємо графік
+            PointF? prevPoint = null;
+            for (float t = -10; t <= 10; t += 0.01f)
+            {
+                float x = a * (t - (float)Math.Sin(t));
+                float y = a * (t - (float)Math.Cos(t));
+
+                float drawX = centerX + x * 40;
+                float drawY = centerY - y * 40;
+
+                PointF currentPoint = new PointF(drawX, drawY);
+
+                if (prevPoint != null)
+                {
+                    g.DrawLine(graphPen, prevPoint.Value, currentPoint);
+                }
+
+                prevPoint = currentPoint;
+            }
+
+            // Підписи осей
+            g.DrawString("X", font, Brushes.Black, width - 20, centerY + 10);
+            g.DrawString("Y", font, Brushes.Black, centerX + 10, 5);
+        }
+
+        // Клік по кнопці ОК
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                a = float.Parse(txtA.Text);
+                picBoxOutput.Invalidate();
+            }
+            catch 
+            {
+                if (string.IsNullOrEmpty(txtA.Text))
+                {
+                    txtA.Focus();
+                }
+            }
+        }
+        // Натискання клавіші в полі введення
+        private void txtA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Правильними символами вважаються цифри,
+            // Кома, <Enter> і <Backspace>.
+            // Будемо вважати правильним символом
+            // також крапку, замінимо її комою.
+            // Інші символи заборонені.
+            // Щоб заборонений символ не відображався
+            // у полі редагування, привласним
+            // значення true властивості Handled параметра e
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9'))
+            {
+                // цифра
+                return;
+            }
+            if (e.KeyChar == '.')
+            {
+                // крапку замінемо комою
+                e.KeyChar = ',';
+            }
+            if (e.KeyChar == ',')
+            {
+                if (txtA.Text.IndexOf(',') != -1)
+                {
+                    // кома вже є в полі редагування
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                // <Enter>, <Backspace>, <Esc>
+                if (e.KeyChar == (char)Keys.Enter)
+                    // натиснута клавіша <Enter>
+                    // встановити курсор на кнопку OK
+                    btnOK.Focus();
+                return;
+            }
+            if (e.KeyChar == '-')
+            {
+                // дозволено мінус
+                return;
+            }
+            // інші символи заборонені
+            e.Handled = true;
+        }
+    }
+}
